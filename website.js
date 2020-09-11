8d2cd9fc3e1e97fd93d3280d8a0a2d19a36e3bc9
block-login <sanju11aryan@gmail.com>
2020-09-11 14:31:29 +0530

    initial commit

---
+++ website.js
@@ -0,0 +1,23 @@
+function initMap() {
+	var location = {lat:76.813615, lng: 30.668612 };
+	var map = new google.maps.map(document.getElementById("map"),{
+		zoom:4,
+		//center:location
+	});
+}
+//AIzaSyCeP0ecbDJl4NWpYy7QEYRyZyyRDUGDFd0
+// async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCeP0ecbDJl4NWpYy7QEYRyZyyRDUGDFd0&callback=initMap"
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
